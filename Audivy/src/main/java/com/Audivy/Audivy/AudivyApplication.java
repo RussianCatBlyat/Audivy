5a12ca73abdf7eb44fc1095438e554c55e7f80e0
RussianCatBlyat <cauab2007@gmail.com>
2025-05-07 16:03:06 -0300

    criação do projeto

---
+++ Audivy/src/main/java/com/Audivy/Audivy/AudivyApplication.java
@@ -0,0 +1,13 @@
+package com.Audivy.Audivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudivyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudivyApplication.class, args);
+	}
+
+}
---
+++ Audivy/src/test/java/com/Audivy/Audivy/AudivyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Audivy.Audivy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudivyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
