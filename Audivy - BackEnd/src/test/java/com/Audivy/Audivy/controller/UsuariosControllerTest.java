47a16b5fd5c0d1718794ee99fe3bc9d929805c7d
RussianCatBlyat <cauab2007@gmail.com>
2025-06-21 12:13:25 -0300

    salvando para almoçar

--- Audivy - BackEnd/src/test/java/com/Audivy/Audivy/controller/UsuariosControllerTest.java
+++ Audivy - BackEnd/src/test/java/com/Audivy/Audivy/controller/UsuariosControllerTest.java
@@ -1,4 +1,52 @@
 package com.Audivy.Audivy.controller;
 
+import com.Audivy.Audivy.models.UsuariosModel;
+import com.Audivy.Audivy.repositories.UsuariosRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static com.Audivy.Audivy.enums.UsuariosPlano.PREMIUM;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class UsuariosControllerTest {
+
+    @InjectMocks
+    private UsuariosController usuariosController;
+
+    @Mock
+    private UsuariosRepository usuariosRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetUsuarioById_UsuarioExiste(){
+        Integer id = 1;
+        UsuariosModel usuario = new UsuariosModel();
+        usuario.setIdUsuario(1);
+        usuario.setNmUsuario("Teste");
+        usuario.setDsEmail("teste@teste.teste");
+        usuario.setDsSenha("12345");
+        usuario.setTpUsuario("artista");
+        usuario.setDsPlano(PREMIUM);
+
+        when(usuariosRepository.findById(id)).thenReturn(Optional.of(usuario));
+
+        ResponseEntity<Object> response = usuariosController.getOneUsuario(id);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertNotNull(response.getBody());
+
+        UsuariosModel usuarioResponse = (UsuariosModel) response.getBody();
+        assertEquals("Teste", usuarioResponse.getNmUsuario());
+    }
 }
